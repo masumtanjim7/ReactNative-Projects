7ab815739ca3f77e4be5b3fb49a995985e2d5b14
Tanjim Hasan Masum <119100205+masumtanjim7@users.noreply.github.com>
2026-02-02 00:45:26 +0600

    RNTestApp

---
+++ RNTestApp/App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet, StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.title}>Hello Masum 👋 React Native is working!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+});
